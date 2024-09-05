c6ec0775b46eead229546d0da1e6d3804a256bc6
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-09-05 22:43:26 +0100

    Split component for high-reuseablity

---
+++ src/app/components/NoProjectSelected.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Button from "./Button";
+
+const NoProjectSelected = () => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <Image
+        src="/logo.png"
+        alt="An empty task list"
+        height={64}
+        width={64}
+        className="object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get stated with a new one
+      </p>
+      <p className="mt-8">
+        <Button>Create new project</Button>
+      </p>
+    </div>
+  );
+};
+
+export default NoProjectSelected;
