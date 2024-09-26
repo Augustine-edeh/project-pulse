7c44c2f166594978c6fdc07654dcd04eb94e839e
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-09-26 19:43:48 +0100

    fix: add proper typing for component props

--- src/app/components/NoProjectSelected.tsx
+++ src/app/components/NoProjectSelected.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import Button from "./Button";
 
-const NoProjectSelected = ({ onStartAddProject }) => {
+interface NoProjectSelectedProps {
+  onStartAddProject: () => void;
+}
+
+const NoProjectSelected = ({ onStartAddProject }: NoProjectSelectedProps) => {
   return (
     <div className="mt-24 text-center w-2/3">
       <Image
@@ -15,7 +19,7 @@ const NoProjectSelected = ({ onStartAddProject }) => {
         No Project Selected
       </h2>
       <p className="text-stone-400 mb-4">
-        Select a project or get stated with a new one
+        Select a project or get started with a new one
       </p>
       <p className="mt-8">
         <Button onClick={onStartAddProject}>Create new project</Button>
