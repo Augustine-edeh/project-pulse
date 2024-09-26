04ea6d1eb3112e86a886c81891ae59ee91e388ba
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-09-26 19:42:21 +0100

    fix: update Button component to use React.ReactNode for children prop type

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,8 +1,8 @@
-interface childrenType {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
 }
 
-const Button = ({ children, ...props }: childrenType) => {
+const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
       className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
